333c801d9a4f12d44540a968a0fd392663e33fc2
Ola Ellnestam <ola.ellnestam@sveaekonomi.se>
2012-11-02 08:18:43 +0100

    A small base to start with

---
+++ src/com/ssem/loan/Application.java
@@ -0,0 +1,15 @@
+package com.ssem.loan;
+
+public class Application {
+
+    private long applicationNo;
+
+    public long getApplicationNo() {
+        return applicationNo;
+    }
+
+    public void setApplicationNo(long applicationNo) {
+        this.applicationNo = applicationNo;
+    }
+
+}
---
+++ src/com/ssem/loan/LoanServer.java
@@ -0,0 +1,12 @@
+package com.ssem.loan;
+
+import org.eclipse.jetty.server.Server;
+
+public class LoanServer {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        server.setHandler(new LoanHandler());
+        server.start();
+        server.join();
+    }
+}
